c84780f5dc32c504d7b70077e322894736af8412
Abhishek Singh Sengar <theabhissengar@gmail.com>
2023-05-11 16:17:06 +0530

    Done

---
+++ player.js
@@ -0,0 +1,19 @@
+export class Player {
+    constructor(game) {
+        this.game = game
+        this.width = 100
+        this.height = 91.3
+        this.x = 0
+        this.y = this.game.height - this.height
+        this.image = document.getElementById('player')
+    }
+    update(input) {
+        if(input.includes('ArrowRight')) this.x++;
+        else if(input.includes('ArrowLeft')) this.x--;
+        else if(input.includes('ArrowUp')) this.y--;
+        else if(input.includes('ArrowDown')) this.y++;
+    }
+    draw(context) {
+        context.drawImage(this.image, 0, 0, this.width, this.height, this.x, this.y, this.width, this.height)
+    }
+}
